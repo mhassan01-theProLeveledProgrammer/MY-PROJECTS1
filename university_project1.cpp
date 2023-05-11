3c1006430bc35def886d6145d0761b4a1557dbc9
Muhammad Hassan Sheikh <hassanjanab36912@gmail.com>
2023-05-12 00:07:47 +0500

    this is a program which is supposed to be used  by my group members who are the boys of the programmers hub aka TPH
    the program which is our first project in the university, till now i have just made a skeleton of it, we will continue it to pro level

---
+++ university_project1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<" WELCOME TO LAHORE GARRISON UNIVERSITY "<<endl;
+    cout<<"          A PROJECT OF PAK ARMY        "<<endl;
+    string name1,name2,name3;
+    cout<<" ENTER YOUR FIRST NAME: "<<endl;
+    //یہاں تک ہم نے کچھ بھی الگ نہیں کیا
+    // I HAVEN'T DID ANY THING DIFFERENT HERE ❤
+    cin>>name1;
+    cout<<" ENTER YOUR MIDDLE NAME: "<<endl;
+    cin>>name2;
+    cout<<" ENTER YOUR LAST NAME: "<<endl;
+    cin>>name3;
+    cout<<" YOUR NAME IS "<<name1<<" "<<name2<<" "<<name3<<endl;
+    int age;
+    cout<<" ENTER YOUR AGE: "<<endl;
+    cin>>age;
+    cout<<" YOUR AGE IS "<<age<<endl;
+    string qualification;
+    cout<<" ENTER YOUR QUALIFICATION TILL NOW "<<endl;
+    cin>>qualification;
+    cout<<" YOUR QUALIFICATION TILL NOW IS "<<qualification<<endl;
+    string address_yours;
+    cout<<" ENTER YOUR ADDRESS WITHOUT SPACES: ";
+    cout<<" JUST WRITE YOUR HOUSE NUMBER, BLOCK, SOCIETY, CITY "<<endl;
+    cin>>address_yours;
+    cout<<" YOUR ADDRESS IS "<<address_yours<<endl;
+    //یہاں تک ہم نے کچھ بھی الگ نہیں کیا
+    int phonenumber;
+    cout<<" ENTER YOUR PHONE NUMBER "<<endl;
+    cin>>phonenumber;
+    cout<<" YOUR PHONE NUMBER IS "<<"0"<<phonenumber;
+     // I HAVEN'T DID ANY THING DIFFERENT HERE ❤
+     return 0;
+}
