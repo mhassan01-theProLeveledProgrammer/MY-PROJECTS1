de7d01652b9720e651647c40b6470e26968ef810
Muhammad Hassan Sheikh <hassanjanab36912@gmail.com>
2023-05-12 14:58:24 +0500

    its  uploaded

--- university_project1.cpp
+++ university_project1.cpp
@@ -18,8 +18,8 @@ FOR THESE FIVE PEOPLE, AS IT IS ACCORDING TO OUR REQUIRMENTS.😊
 using namespace std;
 int main()
 {
-    cout<<" WELCOME TO LAHORE GARRISON UNIVERSITY "<<endl;
-    cout<<"          A PROJECT OF PAK ARMY        "<<endl;
+    cout<<"            WELCOME TO LAHORE GARRISON UNIVERSITY             "<<endl;
+    cout<<"                    A PROJECT OF PAK ARMY             "<<endl;
     string name1,name2,name3;
     cout<<" ENTER YOUR FIRST NAME: "<<endl;
     //یہاں تک ہم نے کچھ بھی الگ نہیں کیا
@@ -51,54 +51,33 @@ int main()
     cin>>address_yours;
     cout<<" YOUR ADDRESS IS "<<address_yours<<endl;
     //یہاں تک ہم نے کچھ بھی الگ نہیں کیا
-    int phonenumber;
-    cout<<" ENTER YOUR PHONE NUMBER "<<endl;
-    cin>>phonenumber;
-    cout<<" YOUR PHONE NUMBER IS "<<"0"<<phonenumber<<endl;
     // I HAVEN'T DID ANY THING DIFFERENT HERE ❤
+
+
     int marks1;
     cout<<" ENTER YOUR MARKS IN MATRIC "<<endl;
     cin>>marks1;
     cout<<" YOUR MARKS IN MATRIC ARE "<<marks1<<endl;
+
+
     int marks2;
     cout<<" ENTER YOUR MARKS IN INTERMEDIATE "<<endl;
     cin>>marks2;
     cout<<" YOUR MARKS IN INTERMEDIATE ARE "<<marks2<<endl;
-    float percent1;
+
+
+   /* float percent1;
     percent1=(marks1/1100)*100;
     cout<<" PERCENTEGE IN MATRIC= "<<percent1;
+
+
     float percent2;
     percent2=(marks2/1100)*100;
-    cout<<" PERCENTAGE IN INTERMEDIATE= "<<percent2;
-    string resulting1=(percent1>=50 && percent2>=50)? " YOU ARE ELIGIBLE TO APPLY " : " YOU ARE NOT ELIGIBLE TO APPLY ";
+    cout<<" PERCENTAGE IN INTERMEDIATE= "<<percent2;*/
+
+
+    string resulting1=(marks1>=500 && marks2>=500)? " YOU ARE ELIGIBLE TO APPLY " : " YOU ARE NOT ELIGIBLE TO APPLY ";
     cout<<resulting1<<endl;  
     return 0;
 
-    /* IN THIS SECTION WE WILL TAKE AN INPUT FROM THE USER AND WILL STORE IT IN THE EXCEL SHEET.
-IT IS STATED THAT THIS SECTION IS ONLY TO MAKE A SKELETON TO STORE DATA IN THE EXCEL SHEET.
-AND OF COURSE WE WILL UPDATE IT SOONER.
-AGAIN I WILL SAY THAT I HAVE USED BASIC CONCEPTS TO CODE IT. 
-BUT HERE WE ARE INTRODUCED WITH SOME NEW THINGS, WHICH YOU GUYS BETTER KNOWS.
-IT IS STATED THAT NO OBJECT-ORIENTED CONCEPT WILL  BE USED IN THIS PROJECT. 
-THIS CODE IS ONLY FOR 
-1.UMER ZULFIQAR 
-2.AMAN MIRZA
-3.SUBHAN ALI 
-4.WALEED IMRAN 
-5.MUHAMMAD HASSAN
-ALL OF THE CODE I HAVE DONE AS A PUBLIC REPOSITORY WITH THE NAME OF "MY-PROJECTS1" IS ONLY AND ONLY
-FOR THESE FIVE PEOPLE, AS IT IS ACCORDING TO OUR REQUIRMENTS.😊  */
-{
-    ofstream outData;
-    string s;
-    cin>>s;
-
-    int n=1;
-
-    outData.open("outfile.csv",ios::app);
-    outData<<s<<endl;
-    outData<<n<<endl;
-    system("pause");
-    return 0;
-}
 }
